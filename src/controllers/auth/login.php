c64487f6c29c50ecf07592ce9b6a411a3f868b78
SantinoPozo <santinospozo@gmail.com>
2026-09-03 21:13:56 -0300

    Logica de login (funcional)

--- src/controllers/auth/login.php
+++ src/controllers/auth/login.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../../config/bootstrap.php';
 
 // Paso clave #1: Validar tipo de solicitud ----------------------
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {  // Si la solicitud no es POST, volves al register
-  header('Location: /src/views/auth/register.php');
+  header('Location: /src/views/auth/login.php');
   exit;
 }
 
@@ -11,59 +11,36 @@ $data = [
   'email'           => trim($_POST['email'] ?? ''), // trim(str) saca los espacios al inicio y al final
   'password'        => $_POST['password'] ?? ''
 ];
-
 try {
   // Validamos que el usuario no exista
   // ...
 
-
-  // Hasheamos la contraseña, nunca se guarda en texto plano
-  $hashedPassword = password_hash($data['password'], PASSWORD_DEFAULT);
-
-  // Insertamos en DB
-  $stmt = $pdo->prepare("SELECT password FROM users WHERE email=:email");
-  $resultado = $stmt->execute([
+  /* Sacamos un registro del DB. */
+  $stmt = $pdo->prepare("SELECT * FROM users WHERE email=:email");
+  
+  $stmt->execute([
     'email'    => $data['email'],
   ]);
-  while($row = $resultado->fetch()) {
-      echo "<tr>";
-      echo "<td>" . $row['id'] . "</td>";
-      echo "<td>" . $row['firstname'] . "</td>";
-      echo "<td>" . $row['lastname'] . "</td>";
-      echo "</tr>";
-    }
-    echo "</table>";
-    unset($result);
-  // Cargamos $_SESSION['user'], para poder pasar al index
-  $_SESSION['user'] = [
-    'id'    => $pdo->lastInsertId(),
-    'name'  => $data['name'],
-    'email' => $data['email'],
-  ];
-try {
-
-  // Process the result set
-  if ($result->rowCount() > 0) {
-    echo "<table><tr><th>ID</th><th>Firstname</th><th>Lastname</th></tr>";
-    // Output data of each row
-    while($row = $result->fetch()) {
-      echo "<tr>";
-      echo "<td>" . $row['id'] . "</td>";
-      echo "<td>" . $row['firstname'] . "</td>";
-      echo "<td>" . $row['lastname'] . "</td>";
-      echo "</tr>";
-    }
-    echo "</table>";
-    unset($result);
-  } else {
-    echo "No records found.";
+  
+  if (false !== ($row = $stmt->fetchColumn())){
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
-} catch(PDOException $e) {
-  echo "Error: " . $e->getMessage();
-}
-  // Pateado para el index
-  header('Location: /src/views/index.php');
-  exit;
+
+  $pass_igual = password_verify($data['password'], $row['password']);
+  if($pass_igual){
+    $_SESSION['user'] = [
+      'id'    => $pdo->lastInsertId(),
+      'name'  => $row['name'],
+      'email' => $data['email'],
+    ];
+    echo "test";
+    header('Location: /src/views/index.php');
+    exit;
+  } else {
+    header('Location: /src/views/auth/login.php');
+    exit;
+  }
+  // Cargamos $_SESSION['user'], para poder pasar al index
 } catch (PDOException $e) {
   exit;
 }
